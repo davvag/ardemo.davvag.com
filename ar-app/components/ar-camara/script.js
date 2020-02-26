b4c7a5b35b466548cd6211b8d2153a005814b428
Lasitha Senanayake <lasitha.senanayake@gmail.com>
2020-02-26 15:13:07 +0530

    add 3

--- ar-app/components/ar-camara/script.js
+++ ar-app/components/ar-camara/script.js
@@ -109,7 +109,7 @@ function initialize()
 	markerRoot1 = new THREE.Group();
 	scene.add(markerRoot1);
 	let markerControls1 = new THREEx.ArMarkerControls(arToolkitContext, markerRoot1, {
-		type: 'pattern', patternUrl: "assets/ar-app/data/pattern-hiru-2.patt",
+		type: 'pattern', patternUrl: "assets/ar-app/data/pattern-hiru-3.patt",
 	})
 
 	let geometry1 = new THREE.PlaneBufferGeometry(1,1, 4,4);
